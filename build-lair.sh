428d8e1fdfe7f0449c1fbc54c963f0a147181771
Connor Turland <connor@sprillow.com>
2021-09-30 06:43:17 -0700

    Update build-lair.sh

--- build-lair.sh
+++ build-lair.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 tag="${GITHUB_REF#refs/tags/}"
 
-cargo install lair_keystore --root . --version 0.0.3
+cargo install lair_keystore --root . --git https://github.com/guillemcordoba/lair --rev 53fea43d53352609978d550f72f987f0ce458896
 
 gh release upload "${tag}" "lair_keystore" --clobber
