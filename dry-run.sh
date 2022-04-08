ce6ddfa169cb9395a0dfa7bb7c451388d3fac5e3
damien <doute.d@gmail.com>
2022-04-08 12:23:23 -0400

    Fixed test data

--- dry-run.sh
+++ dry-run.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-# cargo run --manifest-path crates/holochain-runner/Cargo.toml --release -- --keystore-path test/data/keystore test/profiles.dna test/data/databases
-cargo run --release -- --keystore-path test/data/keystore test/hrea_suite.happ test/data/databases
+cargo run --release -- --keystore-path test/data/keystore test/profiles-test.happ test/data/databases
+#cargo run --release -- --keystore-path test/data/keystore test/hrea_suite.happ test/data/databases
