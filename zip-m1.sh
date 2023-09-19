b7959b3aecf4b38bfc8fb20da97974d5b3d0084b
Connor Turland <connor@sprillow.com>
2023-09-19 10:58:10 -0700

    update zip-m1.sh script

--- zip-m1.sh
+++ zip-m1.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 if [[ $(uname -m) == 'arm64' ]]; then
+  cargo build --release --features sqlite-encrypted
   cd target/release && tar -cvzf holochain-runner-arm64-apple-darwin.tar.gz holochain-runner
+  echo "output: target/release/holochain-runner-arm64-apple-darwin.tar.gz"
+else
+  echo "This script is only for Apple Silicon"
 fi
 
